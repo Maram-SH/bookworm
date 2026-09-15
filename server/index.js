d28bb042f3e4f905be79aa7e1c05905d4c826b52
MariaDoesCoding <126297519+MariaDoesCoding@users.noreply.github.com>
2026-09-15 15:33:22 +0300

    Test POST in index.js

--- server/index.js
+++ server/index.js
@@ -26,6 +26,18 @@ app.get("/api/books", (req, res) => {
   res.json(books)
 });
 
+app.post("/api/books", (req, res) => {
+  const newBook = {
+    id: books.length + 1,
+    title: req.body.title,
+    author: req.body.author
+  };
+
+  books.push(newBook);
+
+  res.status(201).json(newBook)
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 });
