a76cd72b3488e7174d2ab1dfa7f3e396ba521603
MariaDoesCoding <126297519+MariaDoesCoding@users.noreply.github.com>
2026-09-15 15:27:01 +0300

    Experiment with JSON in /api/books

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
-import express from "express";
-
+import express from "express"
 const app = express();
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Bookworm API is running!")
+});
+
+app.get("/api/books", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: "Pride and Prejudice",
+      author: "Jane Austen"
+    },
+    {
+      id: 2,
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien"
+    }
+  ])
 });
 
 app.listen(PORT, () => {
