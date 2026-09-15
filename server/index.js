4c9aa81096c3fd026235652ea7ccc07d1ef59120
MariaDoesCoding <126297519+MariaDoesCoding@users.noreply.github.com>
2026-09-15 15:29:38 +0300

    Create temporary books array

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
 import express from "express"
+
 const app = express();
+app.use(express.json())
 
 const PORT = 3000;
+
+let books = [
+  {
+    id: 1,
+    title: "Pride and Prejudice",
+    author: "Jane Austen"
+  },
+  {
+    id: 2,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien"
+  }
+]
 
 app.get("/", (req, res) => {
   res.send("Bookworm API is running!")
 });
 
 app.get("/api/books", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      title: "Pride and Prejudice",
-      author: "Jane Austen"
-    },
-    {
-      id: 2,
-      title: "The Hobbit",
-      author: "J.R.R. Tolkien"
-    }
-  ])
+  res.json(books)
 });
 
 app.listen(PORT, () => {
