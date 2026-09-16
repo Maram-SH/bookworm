067f666671111ae1635151487e7efe674b7d4eb9
MariaDoesCoding <126297519+MariaDoesCoding@users.noreply.github.com>
2026-09-17 01:45:33 +0300

    Migrate book api to PostgresSQL

--- server/index.js
+++ server/index.js
@@ -40,68 +40,153 @@ app.get("/", (req, res) => {
   res.send("Bookworm API is running!")
 });
 
-app.get("/api/books", (req, res) => {
-  res.json(books)
+app.get("/api/books", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM books")
+
+    const books = result.rows.map((book) => ({
+      id: book.id,
+      title: book.title,
+      author: book.author,
+      currentPage: book.current_page,
+      totalPages: book.total_pages,
+      status: book.status
+    }))
+
+    res.json(books)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Failed to fetch books" })
+  }
 });
 
-app.post("/api/books", (req, res) => {
-  const totalPages = Number(req.body.totalPages)
+app.post("/api/books", async (req, res) => {
+  try {
+    const totalPages = Number(req.body.totalPages)
+    const currentPage = req.body.status === "FINISHED" ? totalPages : 0
 
-  const newBook = {
-    id: books.length + 1,
-    title: req.body.title,
-    author: req.body.author,
-    currentPage: req.body.status === "FINISHED" ? totalPages : 0,
-    totalPages: totalPages,
-    status: req.body.status
-  };
+    const result = await pool.query(`
+      INSERT INTO books (title, author, current_page, total_pages, status)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *`, 
+      [
+        req.body.title,
+        req.body.author,
+        currentPage,
+        totalPages,
+        req.body.status
+      ]
+    )
 
-  books.push(newBook);
+    const book = result.rows[0]
 
-  res.status(201).json(newBook)
+    res.status(201).json({
+      id: book.id,
+      title: book.title,
+      author: book.author,
+      currentPage: book.current_page,
+      totalPages: book.total_pages,
+      status: book.status
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Failed to add book" })
+  }
 })
 
-app.patch("/api/books/:id", (req, res) => {
-  const id = Number(req.params.id)
+app.patch("/api/books/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id)
 
-  const book = books.find((book) => book.id === id)
+    const result = await pool.query(
+      "SELECT * FROM books WHERE id = $1",
+      [id]
+    )
 
-  if (!book) {
-    return res.status(404).json({ error: "Book not found" })
-  }
-
-  if (req.body.currentPage !== undefined) {
-    book.currentPage = req.body.currentPage
-  }
-
-  if (req.body.totalPages !== undefined) {
-    book.totalPages = req.body.totalPages
-  }
-
-  if (req.body.status !== undefined) {
-    book.status = req.body.status
-
-    if (req.body.status === "FINISHED") {
-      book.currentPage = req.body.totalPages
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Book not found" })
     }
-  }
 
-  res.json(book)
+    const book = result.rows[0]
+
+    let currentPage = book.current_page
+    let totalPages = book.total_pages
+    let status = book.status
+
+    if (req.body.currentPage !== undefined) {
+      currentPage = Number(req.body.currentPage)
+    }
+
+    if (req.body.totalPages !== undefined) {
+      totalPages = Number(req.body.totalPages)
+    }
+
+    if (req.body.status !== undefined) {
+      status = req.body.status
+    }
+
+    if (currentPage > totalPages) {
+      return res.status(400).json({
+        error: "Current page cannot be greater than total pages"
+      })
+    }
+
+    if (status === "FINISHED") {
+      currentPage = totalPages
+    }
+
+    const updatedResult = await pool.query(
+      `UPDATE books
+      SET current_page = $1,
+      total_pages = $2,
+      status = $3
+      WHERE id = $4
+      RETURNING *
+      `,
+      [
+        currentPage,
+        totalPages,
+        status,
+        id
+      ]
+    )
+
+    const updatedBook = updatedResult.rows[0]
+
+    res.json({
+      id: updatedBook.id,
+      title: updatedBook.title,
+      author: updatedBook.author,
+      currentPage: updatedBook.current_page,
+      totalPages: updatedBook.total_pages,
+      status: updatedBook.status
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Failed to add book" })
+  }
 })
 
-app.delete("/api/books/:id", (req, res) => {
-  const id = Number(req.params.id)
+app.delete("/api/books/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id)
 
-  const bookExists = books.some((book) => book.id === id)
+    const result = await pool.query(
+      "DELETE FROM books WHERE id = $1 RETURNING *",
+      [id]
+    )
 
-  if (!bookExists) {
-    return res.status(404).json({ error: "Book not found" })
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Book not found" })
+    }
+
+    res.json({ message: "Book deleted successfully" })
+  
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Failed to delete book" })
   }
-
-  books = books.filter((book) => book.id !== id)
-
-  res.json({ message: "Book deleted successfully" })
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
